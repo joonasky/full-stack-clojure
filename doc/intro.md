5a977f88ad1bfd9804939bf6e21501e5d412142b
Joonas Kylliäinen <joonas@giosg.com>
2020-12-17 17:21:42 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to full-stack-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/full_stack_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns full-stack-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [full-stack-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
